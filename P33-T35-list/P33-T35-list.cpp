a67c377f8034ba552722fa263ba4e136680927b7
lapshuntanya <lapshun.tatiana@gmail.com>
2024-11-20 17:46:11 +0200

    main

--- P33-T35-list/P33-T35-list.cpp
+++ P33-T35-list/P33-T35-list.cpp
@@ -2,25 +2,29 @@
 
 int main()
 {
-    // 7 3 9 4
-    using namespace MyList;
+    //// 7 3 9 4
+    //using namespace MyList;
+    //Node<int> first(7); //info=7, next=nullptr
+    //Node<int> second(3); //info=3, next=nullptr
+    //first.next = &second;
+    //Node<int> third(9);//info=9, next=nullptr
+    //second.next = &third;
+    //Node<int> fourth(4);//info=4, next=nullptr
+    //third.next = &fourth;
+    ////------------------------------------------
+    //Node<int>* p = &first;
+    //while (p != nullptr) {
+    //    p->show();
+    //    p = p->next;
+    //}
 
-    Node<int> first(7); //info=7, next=nullptr
+    MyList::List<int> a;
+    a.showList();
 
-    Node<int> second(3); //info=3, next=nullptr
-    first.next = &second;
-
-    Node<int> third(9);//info=9, next=nullptr
-    second.next = &third;
-
-    Node<int> fourth(4);//info=4, next=nullptr
-    third.next = &fourth;
-
-    //------------------------------------------
-    Node<int>* p = &first;
-    while (p != nullptr) {
-        p->show();
-        p = p->next;
-    }
+    a.push_back(9);
+    a.push_back(3);
+    a.push_back(7);
+    a.push_back(4);
+    a.showList();
 }
 
