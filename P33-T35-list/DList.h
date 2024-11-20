266d71ba947eabe75dd323d4005a45e0a020fc1d
lapshuntanya <lapshun.tatiana@gmail.com>
2024-11-20 18:34:08 +0200

    DList

--- P33-T35-list/DList.h
+++ P33-T35-list/DList.h
@@ -17,3 +17,38 @@ public:
 		cout << "\tnext: " << next << endl;
 	}
 };
+
+
+template<typename T>
+class DList {
+	DNode<T>* head;
+	DNode<T>* current;
+public:
+	DList() { head = current = nullptr; }
+
+	void push_back(T value) {
+		//1 Створюємо новий вузол
+		DNode<T>* el = new DNode<T>(value);
+		if (head == nullptr) { //це перший вузол
+			head = el;
+			current = el;
+		}
+		else {//це НЕ перший вузол
+			//привязка вузлів
+			current->next = el; //зберігаємо адресу нового вузла
+			el->prev = current;
+			current = el;//оновлення поточного вузла
+		}
+	}
+
+	void showList() {
+		if (head == nullptr) cout << "List is empty\n";
+		else {
+			Node<T>* p = head;
+			while (p != nullptr) {
+				p->show();
+				p = p->next;
+			}
+		}
+	}
+};
--- P33-T35-list/P33-T35-list.cpp
+++ P33-T35-list/P33-T35-list.cpp
@@ -1,4 +1,5 @@
 #include "My-List.h"
+#include "DList.h"
 
 int main()
 {
@@ -18,6 +19,7 @@ int main()
     //    p = p->next;
     //}
 
+    /*
     MyList::List<int> a;
     a.showList();
 
@@ -25,6 +27,30 @@ int main()
     a.push_back(3);
     a.push_back(7);
     a.push_back(4);
-    a.showList();
+    a.showList();*/
+
+    /*DNode<int> first(7);
+
+    DNode<int> second(3);
+    first.next = &second;
+    second.prev = &first;
+
+    DNode<int> third(4);
+    second.next = &third;
+    third.prev = &second;
+
+    DNode<int> fourth(9);
+    third.next = &fourth;
+    fourth.prev = &third;
+   
+    // Вивести в прямому //7 3 4 9
+    DNode<int>* p = &first;
+    while (p != nullptr) { cout << p->info; p = p->next; }
+
+    //та зворотньому порядку //9 4 3 7
+    p = &fourth;
+    while (p != nullptr) { cout << p->info; p = p->prev; } */
+    
+
 }
 
